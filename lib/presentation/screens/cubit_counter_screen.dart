06123d4dcc7dab8edc1253412b5a0b7ae95dad64
josemlegal <josemlegal97@gmail.com>
2023-08-04 20:09:47 -0400

    Consume Counter State

--- lib/presentation/screens/cubit_counter_screen.dart
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/counter_cubit/counter_cubit.dart';
 
 class CubitCounterScreen extends StatelessWidget {
   const CubitCounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (context) => CounterCubit(), child: const _CubitCounterView());
+  }
+}
+
+class _CubitCounterView extends StatelessWidget {
+  const _CubitCounterView();
+
+  @override
+  Widget build(BuildContext context) {
+    final counterState = context.watch<CounterCubit>().state;
+
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Cubit Counter"),
+          centerTitle: true,
+          title: Text("Cubit Counter: ${counterState.transactionCount}"),
           actions: [
             IconButton(
               onPressed: () {},
@@ -15,8 +30,12 @@ class CubitCounterScreen extends StatelessWidget {
             ),
           ],
         ),
-        body: const Center(
-          child: Text("Counter value: x"),
+        body: Center(
+          child: BlocBuilder<CounterCubit, CounterState>(
+            builder: (context, state) {
+              return Text("Counter value: ${state.counter}");
+            },
+          ),
         ),
         floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
